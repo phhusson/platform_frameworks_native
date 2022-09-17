phhusson/platform_frameworks_native

commit 53b95fca57d2325fd517340dac2e924d420fa8a3

Try to fallback mCallingSid to getpidcon
This is needed because old vendors (before April 2019 ~) don't support
reporting SELinux context of the caller.
This doesn't work for all processes, because it requires some additional
SELinux permissions. At the moment, only keystore2 requires this

Change-Id: I3b6c4dac9d0e20a3d66f931b283e3a535ab499cd

libs/binder/IPCThreadState.cpp
@@ -36,6 +36,7 @@
 #include <sched.h>
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <sys/resource.h>
 #include <unistd.h>
@@ -1246,6 +1247,25 @@ status_t IPCThreadState::executeCommand(int32_t cmd)
             mCallingUid = tr.sender_euid;
             mLastTransactionBinderFlags = tr.flags;
 
+            // This is recoding libselinux's getpidcon()
+            // We are in a NDK lib, so we need to keep changes to a minimum
+            bool allocatedSid = false;
+            if(mCallingSid == nullptr && mCallingPid != 0) {
+                char buf[4096];
+                char *path = NULL;
+                (void)asprintf(&path, "/proc/%d/attr/current", mCallingPid);
+                int fd = open(path, O_RDONLY | O_CLOEXEC);
+                if(fd != -1) {
+                    int readRet = read(fd, buf, sizeof(buf)-1);
+                    if(readRet != -1) {
+                        buf[readRet] = 0;
+                        mCallingSid = strdup(buf);
+                        allocatedSid = true;
+                    }
+                    close(fd);
+                }
+            }
+
             // ALOGI(">>>> TRANSACT from pid %d sid %s uid %d\n", mCallingPid,
             //    (mCallingSid ? mCallingSid : "<N/A>"), mCallingUid);
 
@@ -1310,6 +1330,7 @@ status_t IPCThreadState::executeCommand(int32_t cmd)
 
             mServingStackPointer = origServingStackPointer;
             mCallingPid = origPid;
+	    if(allocatedSid) free((void*)mCallingSid);
             mCallingSid = origSid;
             mCallingUid = origUid;
             mStrictModePolicy = origStrictModePolicy;

commit f8bf40196274a9923f370ba95ce1a6d55cf448f8

Fix memory leak on "Try to fallback mCallingSid to getpidcon"
Also fixes styling to stay consistent with the rest of the file.

Fixes issue https://github.com/phhusson/treble_experimentations/issues/2410
Commit 53b95fca57d2325fd517340dac2e924d420fa8a3 introduced this memory leak.

libs/binder/IPCThreadState.cpp
@@ -1250,12 +1250,12 @@ status_t IPCThreadState::executeCommand(int32_t cmd)
             // This is recoding libselinux's getpidcon()
             // We are in a NDK lib, so we need to keep changes to a minimum
             bool allocatedSid = false;
-            if(mCallingSid == nullptr && mCallingPid != 0) {
+            if (mCallingSid == nullptr && mCallingPid != 0) {
                 char buf[4096];
                 char *path = NULL;
                 (void)asprintf(&path, "/proc/%d/attr/current", mCallingPid);
                 int fd = open(path, O_RDONLY | O_CLOEXEC);
-                if(fd != -1) {
+                if (fd != -1) {
                     int readRet = read(fd, buf, sizeof(buf)-1);
                     if(readRet != -1) {
                         buf[readRet] = 0;
@@ -1264,6 +1264,7 @@ status_t IPCThreadState::executeCommand(int32_t cmd)
                     }
                     close(fd);
                 }
+                free(path);
             }
 
             // ALOGI(">>>> TRANSACT from pid %d sid %s uid %d\n", mCallingPid,
@@ -1330,7 +1331,7 @@ status_t IPCThreadState::executeCommand(int32_t cmd)
 
             mServingStackPointer = origServingStackPointer;
             mCallingPid = origPid;
-	    if(allocatedSid) free((void*)mCallingSid);
+            if (allocatedSid) free((void*)mCallingSid);
             mCallingSid = origSid;
             mCallingUid = origUid;
             mStrictModePolicy = origStrictModePolicy;